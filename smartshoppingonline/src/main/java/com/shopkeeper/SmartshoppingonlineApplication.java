dddbb5d1429d4dee1ee0179da7e2a3cc2e2ca6ca
vykuntarao.erothu <vykuntarao.erothu@DS-PC01XP6S.HCLT.CORP.HCL.IN>
2020-02-27 15:32:09 +0530

    first commit

---
+++ smartshoppingonline/src/main/java/com/shopkeeper/SmartshoppingonlineApplication.java
@@ -0,0 +1,13 @@
+package com.shopkeeper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartshoppingonlineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartshoppingonlineApplication.class, args);
+	}
+
+}
---
+++ smartshoppingonline/src/test/java/com/shopkeeper/SmartshoppingonlineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shopkeeper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartshoppingonlineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
